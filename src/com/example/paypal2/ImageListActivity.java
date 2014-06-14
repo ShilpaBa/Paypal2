4fbea1af41d5e5733cf3fe900ec08f15ae24a5ce
ShilpaBa <gujjarshilpa1980@gmail.com>
2014-06-13 22:48:04 -0700

    Submitted by Shilpa Bambore for PayPal TakeHome assignment

--- src/com/example/paypal2/ImageListActivity.java
+++ src/com/example/paypal2/ImageListActivity.java
@@ -1,53 +1,53 @@
 package com.example.paypal2;
 
 import android.app.Activity;
-import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.GridView;
-import android.widget.RadioGroup;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
 
 /**
- * This class shows all the downloaded images in grid view to user 
+ * This class shows all the downloaded images in grid view to user
  * 
  * @author Shilpa Bambore
  */
 public class ImageListActivity extends Activity {
 
 	@Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        Log.d("onCreate", "before setting load");
-        setContentView(R.layout.activity_image_list);
- 
-        GridView gridView = (GridView) findViewById(R.id.grid_view);
- 
-        Log.d("onCreate", "starting image download");
-        // Instance of ImageAdapter Class
-        gridView.setAdapter(new ImageAdapter());
-        
-        Log.d("onCreate", "download load completed");
- 
-        /**
-         * On Click event for Single Gridview Item
-         * */
-        gridView.setOnItemClickListener(new OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View v,
-                    int position, long id) {
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-            	Log.d("setOnItemClickListener", "clicked image - "+ position + "  id - " + id);
-                // Sending image id to FullScreenActivity
-                Intent i = new Intent(getApplicationContext(), ImageActivity.class);
-                // passing array index
-                i.putExtra("id", position);
-                startActivity(i); 
-            }
-        });
-    }
+		Log.d("onCreate", "before setting load");
+		setContentView(R.layout.activity_image_list);
+
+		GridView gridView = (GridView) findViewById(R.id.grid_view);
+
+		Log.d("onCreate", "starting image download");
+		// Instance of ImageAdapter Class
+		gridView.setAdapter(new ImageAdapter());
+
+		Log.d("onCreate", "download load completed");
+
+		/**
+		 * On Click event for Single Gridview Item
+		 * */
+		gridView.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View v,
+					int position, long id) {
+
+				Log.d("setOnItemClickListener", "clicked image - " + position
+						+ "  id - " + id);
+				// Sending image id to FullScreenActivity
+				Intent i = new Intent(getApplicationContext(),
+						ImageActivity.class);
+				// passing array index
+				i.putExtra("id", position);
+				startActivity(i);
+			}
+		});
+	}
 }
